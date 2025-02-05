7d764e121c5310e78bad8255183d04b3397f601c
ChuchitaFuentes <fuentes.nunez.maria.prog@gmail.com>
2025-02-04 20:12:39 -0600

    Se agregaron comandos en el documento .js para ver los diferentes tipos de variables

---
+++ JAVASCRIPT_DESDE_0/250204_CLASE/tipos-de-datos.js
@@ -0,0 +1,28 @@
+//ESte es un comentario en Java Script
+//console.log es una funcion que e ituliza para imprimir en consola
+console.log("Hola mundo");
+//Declaración de variables
+ var nombre = "Ana"; //String, tambien se pueden usar comillas simples
+ var edad = 23 //Number
+ var sueldo = 1.234; //Number
+ var casada = false; //Boolean
+ var vacunas = true; //Boolean
+ var hijos = null; //Boolean
+ var dato; //Undefined
+ 
+ console.log(typeof(nombre));
+ console.log(typeof(edad));
+ console.log(typeof(sueldo));
+ console.log(typeof(casada));
+ console.log(typeof(vacunas));
+ console.log(typeof(hijos));
+ console.log(typeof(dato));
+ //La semántica es importante, significado de las palabras
+//NaN (Not a Numbre) es un valor especial que indica que una operacion matemática no es un número
+console.log (10*"Ana")
+ /*Si ponemos typeof de null dirá que es objeto, pero en realidad es usenca intencional de valor. Esto es debido a un error en Java Script que no se ha corregido por compatibilidad con versiones anteriores.*/
+
+ //Ejercicios de campus
+
+ console.log(typeof(42));
+ console.log(typeof("25"));
