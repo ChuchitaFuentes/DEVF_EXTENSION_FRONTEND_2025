44e95e3dfdd1dedf2de750f592827cdf3a955cc4
ChuchitaFuentes <fuentes.nunez.maria.prog@gmail.com>
2025-02-06 19:41:37 -0600

    Se agrega un documento js con los operadores básicos

---
+++ JAVASCRIPT_DESDE_0/250206_CLASE/operadores-basicos.js
@@ -0,0 +1,67 @@
+//Declaro 2 valores
+let valor_1 = 27;
+let valor_2 = 4;
+
+//Operaciones básicas
+let suma = valor_1 + valor_2;
+console.log(suma);
+
+let resta = valor_1 - valor_2;
+console.log(resta);
+
+let multiplicacion = valor_1 * valor_2;
+console.log(multiplicacion);
+
+let division = valor_1 / valor_2;
+console.log(division);
+
+let modulo = valor_1 % valor_2;
+console.log(modulo);
+
+let exponencial = valor_1 ** valor_2;
+console.log(exponencial);
+
+//Operadores de comparación
+
+let numero_1 = 3; //typeof number
+let numero_2 = "3"; //typeof string
+
+console.log(numero_1 == numero_2); //Comparación débil
+
+console.log(numero_1 === numero_2); //Comparación estricta
+
+console.log("Operadores lógicos y de comparación");
+// La respuesta es un operador booleano
+console.log(10>4);
+console.log (10<1);
+console.log(10>=10);
+console.log (10<=10);
+
+console.log("Diferente de:");
+console.log(15 != 15); //False, porque es el mismo valor
+
+console.log("AND: && ");
+ //Para poder manejar
+let edad =20;
+let tieneLicencia =true;
+console.log(edad>=18 && tieneLicencia);
+
+console.log("OR: || ");
+
+//Para aprender a programas
+let esMayorDeEdad = false;
+let esEstudiante = true;
+console.log(esMayorDeEdad || esEstudiante);
+
+console.log("NOT: ! ");
+let esFinDeSemana=false;
+console.log(!esFinDeSemana);
+
+let calificacion = 9;
+let calificacion1 = 10;
+let calificacion2 = 3;
+let calificacion3 = 7;
+let calificacion4 = 8;
+
+//+= Nos permite hacer una suma acumulativa promedio = promedio + calificacion1 es igual a promedio += calificacion1
+
