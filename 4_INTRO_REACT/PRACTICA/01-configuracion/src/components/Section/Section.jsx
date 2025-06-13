1fdbc843484d4f47c0207a0cffa54383ae399dca
ChuchitaFuentes <fuentes.nunez.maria.prog@gmail.com>
2025-06-12 19:55:05 -0600

    Se agregan archivos de imagen y una simulacion de una pagina de perfiles

--- 4_INTRO_REACT/PRACTICA/01-configuracion/src/components/Section/Section.jsx
+++ 4_INTRO_REACT/PRACTICA/01-configuracion/src/components/Section/Section.jsx
@@ -1,11 +1,41 @@
-const frutas =['Manzana',  'Pera', 'Naranja']; /* Se simula la peticion a un servidor */
+import userImg from '../../assets/usuario.png'
+import './Section.css'
+const users =[
+    {
+        id: 1,
+        name: 'Chuchita',
+        description: 'Persona chistosa',
+        image: userImg
+    },
+     {
+        id: 2,
+        name: 'Eli',
+        description: 'Persona divertida',
+        image: userImg
+    },
+     {
+        id: 3,
+        name: 'Tere',
+        description: 'Persona amorosa',
+        image: userImg
+    }
+]
 export const Section = () => {
   return (
-    <ul>
+    <section>
         {
-            frutas.map((fruta)=> <li key ={fruta}>{fruta}</li>)
+            users.map(user=>{
+                return(
+                    <div className='card'key ={user.id}>
+                        <img className='image' src={user.image} alt={user.name} />
+                        <h2 className='name'>{user.name}</h2>
+                        <p className='description'>{user.description}</p>
+                    </div>
+                )
+            })
         }
-    </ul>
+    </section>
   )
 }
+
 
