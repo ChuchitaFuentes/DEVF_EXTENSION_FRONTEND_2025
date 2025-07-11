a769efd8d235b49ca1e43af8784023f29f77e8d2
ChuchitaFuentes <fuentes.nunez.maria.prog@gmail.com>
2025-07-10 20:36:52 -0600

    Se modifica para llamar API de RIck y Morty

--- 5_REACT_AVANZADO/01_CLASE/mi-blog/src/components/ArticulosBlog.jsx
+++ 5_REACT_AVANZADO/01_CLASE/mi-blog/src/components/ArticulosBlog.jsx
@@ -4,31 +4,32 @@ import '../components/ArticulosBlog.css'
 
 
 export default function ArticulosBlog ()  {
-    const [articulos, setArticulos] = useState([]); /* los corchetes se ponen para señalar que es un arreglo */
+    const [characters, setCharacters] = useState([]); /* los corchetes se ponen para señalar que es un arreglo */
     const [isLoading, setIsLoading] = useState(true); /*se pone como boolean porque su estado inicial te dice si esta cargando o no, y es true porque sino no comenzaria */
     const [error, setError] = useState (null); /* Porque no hay errores en un inicio */
 
     useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+        fetch('https://rickandmortyapi.com/api/character?page=2') /* Se modifica page=2 para cambiar la pagina */
             .then (res => {
                 if (!res.ok) throw new Error('Error al obtener los artículos')
                 return res.json(); /* que en cuanto lo lea lo tranforme en un formato json lo que abajo se usa como data*/
             })
-            .then(data => setArticulos(data))
+            .then(data => setCharacters(data.results))
             .catch(err => setError(err))
             .finally(()=>setIsLoading(false))
     }, []);
 
-    if (isLoading) return <h1>Cargando artículos...</h1>
+    if (isLoading) return <h1>Cargando personajes...</h1>
     if (error) return <h1>Error: {error}</h1>
 
     return (
     <div className="articulos">
-        <h2>Artículos</h2>
-        {articulos.map(articulo => (
-            <div className="card"key={articulo.id}>
-                <h3>{articulo.title}</h3> 
-                <p>{articulo.body}</p>
+        <h2>Personakes de Rick and Morty</h2>
+        {characters.map(character => (
+            <div className="card"key={character.id}>
+                <h3>{character.name}</h3> 
+                <p>{character.species}</p>
+                <img src={character.image} alt="" />
             </div>
         ))}
     </div>
