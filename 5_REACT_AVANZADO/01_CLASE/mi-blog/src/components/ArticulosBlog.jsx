22c1e2a4a9777045ac9c974368e07b0b3a5e62f2
ChuchitaFuentes <fuentes.nunez.maria.prog@gmail.com>
2025-07-10 21:26:08 -0600

    Se modifica el código para consumo de una API de recetas.

--- 5_REACT_AVANZADO/01_CLASE/mi-blog/src/components/ArticulosBlog.jsx
+++ 5_REACT_AVANZADO/01_CLASE/mi-blog/src/components/ArticulosBlog.jsx
@@ -4,32 +4,38 @@ import '../components/ArticulosBlog.css'
 
 
 export default function ArticulosBlog ()  {
-    const [characters, setCharacters] = useState([]); /* los corchetes se ponen para señalar que es un arreglo */
+    const [recipes,setRecipes] = useState([]); /* los corchetes se ponen para señalar que es un arreglo */
     const [isLoading, setIsLoading] = useState(true); /*se pone como boolean porque su estado inicial te dice si esta cargando o no, y es true porque sino no comenzaria */
     const [error, setError] = useState (null); /* Porque no hay errores en un inicio */
 
     useEffect(()=>{
-        fetch('https://rickandmortyapi.com/api/character?page=2') /* Se modifica page=2 para cambiar la pagina */
+        fetch('https://dummyjson.com/recipes?limit=10') /* Se modifica page=2 para cambiar la pagina */
             .then (res => {
                 if (!res.ok) throw new Error('Error al obtener los artículos')
                 return res.json(); /* que en cuanto lo lea lo tranforme en un formato json lo que abajo se usa como data*/
             })
-            .then(data => setCharacters(data.results))
+            .then(data => setRecipes(data.recipes))
             .catch(err => setError(err))
             .finally(()=>setIsLoading(false))
     }, []);
 
-    if (isLoading) return <h1>Cargando personajes...</h1>
+    if (isLoading) return <h1>Cargando recetas...</h1>
     if (error) return <h1>Error: {error}</h1>
 
     return (
     <div className="articulos">
-        <h2>Personakes de Rick and Morty</h2>
-        {characters.map(character => (
-            <div className="card"key={character.id}>
-                <h3>{character.name}</h3> 
-                <p>{character.species}</p>
-                <img src={character.image} alt="" />
+        <h2>Popular Recipes</h2>
+        {recipes.map(recipe => (
+            <div className="card"key={recipe.id}>
+                <h3>{recipe.name}</h3> 
+                <p>{recipe.species}</p>
+                <ul>Ingredients:
+                {(recipe.ingredients).map(ingredient=>(<li>{ingredient}</li>))}
+                </ul>
+                <ol>Instructions:
+                {(recipe.instructions).map(instruction=>(<li >{instruction}</li>))}
+                </ol>
+                <h4>{recipe.difficulty}</h4>
             </div>
         ))}
     </div>
