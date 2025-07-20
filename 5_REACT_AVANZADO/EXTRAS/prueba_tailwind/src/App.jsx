f6fb5be63bc36fffc4b16cebc87779fcf5ccbff8
ChuchitaFuentes <fuentes.nunez.maria.prog@gmail.com>
2025-07-20 16:07:08 -0600

    Manejo de grid

--- 5_REACT_AVANZADO/EXTRAS/prueba_tailwind/src/App.jsx
+++ 5_REACT_AVANZADO/EXTRAS/prueba_tailwind/src/App.jsx
@@ -3,13 +3,17 @@ import { useState } from 'react'
 
 
 function App() {
- 
+
 
   return (
     <div className='p-10'>
-      <div className='border-2 flex gap-2' >
-        <div className="box w-40 ">01</div>
-        <div className="box w-80">02</div>
+      <div className='grid grid-cols-6 gap-2 grid-rows-8'>
+        <div className="box col-span-4 col-start-2">01</div>
+        <div className="box col-start-1 col-end-3">02</div>
+        <div className="box col-span-2 col-end-7">03</div>
+        <div className="box col-start-1 col-end-7">04</div>
+        <div className="box col-span-full row-start-5">05</div>
+        
         
       </div>
     </div>
