d4105fd6cccfb8bd7b37ce7a7b463617a7545b36
ChuchitaFuentes <fuentes.nunez.maria.prog@gmail.com>
2025-07-20 15:50:30 -0600

    FlexBox

--- 5_REACT_AVANZADO/EXTRAS/prueba_tailwind/src/App.jsx
+++ 5_REACT_AVANZADO/EXTRAS/prueba_tailwind/src/App.jsx
@@ -6,10 +6,13 @@ function App() {
  
 
   return (
-    <>
-      <p className='bg-amber-500 w-40 border-8 border-blue-700 p-1.5 m-4 box-content'>Caja 1</p>
-      <span className='bg-amber-500 w-40 border-8 border-blue-700 p-1.5 m-4 box-content inline-block'>Caja 2</span>
-    </>
+    <div className='p-10'>
+      <div className='border-2 flex gap-2' >
+        <div className="box w-40 ">01</div>
+        <div className="box w-80">02</div>
+        
+      </div>
+    </div>
   )
 }
 
