f50b713ac58207dda3223e00b15d193d8f494653
ChuchitaFuentes <fuentes.nunez.maria.prog@gmail.com>
2025-07-20 15:31:03 -0600

    Box model

--- 5_REACT_AVANZADO/EXTRAS/prueba_tailwind/src/App.jsx
+++ 5_REACT_AVANZADO/EXTRAS/prueba_tailwind/src/App.jsx
@@ -7,16 +7,8 @@ function App() {
 
   return (
     <>
-      {/* Ancho alto  */}
-      <div className='bg-amber-600 w-xs'>Medidas - TailWind 4.0</div> {/*Medidas preestablecidas */}
-      <div className='bg-sky-600 w-[390px]'>Medidas - TailWind 4.0</div> {/*Medidas personalizadas */}
-      <div className='bg-gray-600 size-[390px]'>Medidas - TailWind 4.0</div>{/* Medidas iguales para alto y ancho */}
-      <div className='bg-blue-600 h-card'>Medidas - TailWind 4.0</div>{/* Mediddas definidas por varible dentro del css */}
-      {/* Tamaño en fracciones dentro de un flex */}
-      <div className='flex gap-0.5 p-2'>
-        <div className='bg-indigo-400 w-1/3'>Elemento 1</div>
-        <div className='bg-indigo-600 w-2/3'>Elemento 2</div>
-      </div>
+      <p className='bg-amber-500 w-40 border-8 border-blue-700 p-1.5 m-4 box-content'>Caja 1</p>
+      <span className='bg-amber-500 w-40 border-8 border-blue-700 p-1.5 m-4 box-content inline-block'>Caja 2</span>
     </>
   )
 }
