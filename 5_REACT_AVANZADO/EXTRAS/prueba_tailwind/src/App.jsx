bc52d5ca607c3e14735effe51598122e28c5ba08
ChuchitaFuentes <fuentes.nunez.maria.prog@gmail.com>
2025-07-20 19:03:08 -0600

    Querys, manejo responsivo basico

--- 5_REACT_AVANZADO/EXTRAS/prueba_tailwind/src/App.jsx
+++ 5_REACT_AVANZADO/EXTRAS/prueba_tailwind/src/App.jsx
@@ -6,17 +6,21 @@ function App() {
 
 
   return (
-    <div className='p-10'>
-      <div className='grid grid-cols-6 gap-2 grid-rows-8'>
-        <div className="box col-span-4 col-start-2">01</div>
-        <div className="box col-start-1 col-end-3">02</div>
-        <div className="box col-span-2 col-end-7">03</div>
-        <div className="box col-start-1 col-end-7">04</div>
-        <div className="box col-span-full row-start-5">05</div>
-        
-        
+    /* Sm y md son puntos de quiebre para manejo del diseño responsivo */
+    <>
+      <h1 className='font-bold text-2xl sm:text-3xl md:text-4xl'>Curso Tailwind</h1> {/* tamaños predeterminados */}
+      <h1 className='font-bold text-2xl min-[650px]:text-3xl min-[800px]:text-4xl'>Curso Tailwind</h1> {/* Tamaños personalizados */}
+      <h1 className='sm:max-md:bg-amber-400'>Texto con fondo</h1> {/* Este es un breakpoint con rango */}
+
+      <div className='p-4 grid grid-cols-2 gap-2 tablet:grid-cols-3 desktop:grid-cols-4'>
+        <div className="box">1</div>
+        <div className="box">2</div>
+        <div className="box">3</div>
+        <div className="box">4</div>
+        <div className="box">5</div>
+        <div className="box">6</div>
       </div>
-    </div>
+    </>
   )
 }
 
