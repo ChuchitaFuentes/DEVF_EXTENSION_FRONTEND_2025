d43ce9e2c224b23305a3d28325eda537b4a5a709
ChuchitaFuentes <fuentes.nunez.maria.prog@gmail.com>
2025-07-20 19:38:52 -0600

    Pseudo Clases

--- 5_REACT_AVANZADO/EXTRAS/prueba_tailwind/src/App.jsx
+++ 5_REACT_AVANZADO/EXTRAS/prueba_tailwind/src/App.jsx
@@ -8,17 +8,20 @@ function App() {
   return (
    
     <>
-      <h1 className='text-center font-bold mb-4 text-5xl'>Curso Tailwind 4.0</h1> 
-      <h2 className='text-center font-bold mb-10 text-4xl'>Container Queries</h2>
-      
-
-      <div className='@container border-2 border-red-500 p-4 resize overflow-hidden'> {/* Overflow, te dice que hacer con el contenido y el resize sirve para poder cambiar de tamaño */}
-        <div className='flex flex-col gap-2 @sm:flex-row'> {/* para cambiar el tamaño con respecto a el contenedor @container */}
-          <div className="box">1</div>
-          <div className="box">2</div>
-          <div className="box">3</div>
-        </div>
-      </div>
+      <h1 className='text-center font-bold mb-4 text-5xl'>Curso Tailwind 4.1</h1> 
+      <h2 className='text-center font-bold mb-10 text-4xl'>Pseudo clases</h2>
+       <button className='bg-blue-800 text-white px-5 py-2 rounded-full font-semibold hover:bg-blue-300 cursor-pointer focus:outline-2 focus:outline-red-400 active:bg-amber-400 mb-4'>Aceptar</button>
+       <div className='mb-4'>
+        <a href="https://www.google.com" className="visited:text-green-500" target="_blank">Google</a>
+        <a href="https://www.github.com" className="visited:text-green-500" target="_blank">Github</a>
+        <a href="https://www.youtube.com" className="visited:text-green-500" target="_blank">Youtube</a>
+       </div>
+       <ul className='mb-4'>
+        <li className="first:bg-amber-300 last:bg-red-500 only:bg-blue-300">HTML</li>
+        <li className="first:bg-amber-300 last:bg-red-500 only:bg-blue-300">CSS</li>
+        <li className="first:bg-amber-300 last:bg-red-500 only:bg-blue-300">Tailwind</li>
+       </ul>
+       <input type="text" placeholder='Nombre:' className='disabled:bg-gray-300 required:border-red-500' />
     </>
   )
 }
