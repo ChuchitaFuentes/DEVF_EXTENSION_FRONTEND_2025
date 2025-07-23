c46a7392e6db71d29b22d05f0bb2d091a1370468
ChuchitaFuentes <fuentes.nunez.maria.prog@gmail.com>
2025-07-22 18:30:19 -0600

    En proceso, se va a implementar posteriormente el dark mode

--- 5_REACT_AVANZADO/EXTRAS/prueba_tailwind/src/App.jsx
+++ 5_REACT_AVANZADO/EXTRAS/prueba_tailwind/src/App.jsx
@@ -8,9 +8,14 @@ function App() {
   return (
    
     <>
-      <button className='button'>Botón 1</button>
-      <button  className='button'>Botón 2</button>
-      <button className='button'>Botón 3</button>
+    <div className='flex place-content-end'>
+      <button id='themeButton' className='border-2 border-gray-400 tex'></button>
+    </div>
+      <div className='dark:bg-blue-950 dark:text-emerald-100 m-4 p-4 border-2 rounded-2xl '>
+        <h1 className='text-3xl'>Este es un título</h1>
+        <h2 className='text-2xl'>Este es un subtítulo</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae enim illo perferendis repudiandae distinctio iste amet praesentium doloremque excepturi, velit ab rerum, quo deleniti, modi hic itaque. Repellendus, veritatis est.</p>
+      </div>
     </>
   )
 }
